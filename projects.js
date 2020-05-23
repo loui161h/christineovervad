6fc4f83f10a25f444f3d4bdc18352feab404b7a6
loui161h <55078332+loui161h@users.noreply.github.com>
2020-05-23 17:59:33 +0200

    projects klar og burger klar

---
+++ projects.js
@@ -0,0 +1,51 @@
+ const endpoint = "http://louisesettrup.dk/kea/wordpress/babushka/wp-json/wp/v2/ret";
+ let projects = [];
+ let filter = "alle";
+ const container = document.querySelector("main");
+ const projectsTemplate = document.querySelector("template");
+ /* const detalje = document.querySelector("#detalje");*/
+
+
+ document.addEventListener("DOMContentLoaded", start);
+
+ function start() {
+     hentData();
+     addEventListenersToButtons();
+ }
+
+ async function hentData() {
+     const response = await fetch(endpoint);
+     console.log(response);
+     projects = await response.json();
+     console.log(projects);
+     visProjects();
+
+ }
+
+ //data fra array ligger i json fil - alledyr.json
+
+ function visProjects() {
+     container.innerHTML = "";
+
+
+     projects.forEach((project) => {
+         /* først henter data i json fil ret.content.rendered - efter pods hedder rettype fordi det hedder den i filtreringen i PODS */
+         if (filter == "alle" || filter == project.rettype) {
+             let klon = projectsTemplate.cloneNode(true).content;
+
+             /* HENTET FRA PODS */
+             klon.querySelector("img").src = project.billede.guid;
+
+
+
+             klon.querySelector(".projects").addEventListener("click", () => {
+                 visDetalje(project)
+             });
+
+
+
+
+             container.appendChild(klon);
+         }
+     })
+ }
