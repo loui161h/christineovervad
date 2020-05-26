414a33d4e54a9bb723ba52751af7e34df2eeaf38
Cecilie Slemming <bruger@Brugers-MacBook-Pro.local>
2020-05-26 13:04:47 +0200

    press done, mangler kun links

--- press.js
+++ press.js
@@ -22,21 +22,33 @@
       document.querySelector("#press_heading").textContent = press.title.rendered;
       /* HUSK VARIBEL HER */
       document.querySelector("#press_reviews").textContent = press.reviews;
+      document.querySelector("#review_img").src = press.review_img.guid;
       document.querySelector("#press_review1").textContent = press.review1;
       document.querySelector("#press_review_year1").textContent = press.review_year1;
 
+
       document.querySelector("#press_review2").textContent = press.review2;
+        document.querySelector("#review2_img").src = press.review2_img.guid;
       document.querySelector("#press_review_year2").textContent = press.review_year2;
+
+
       document.querySelector("#press_interviews").textContent = press.interviews;
+      document.querySelector("#interview_img").src = press.interview_img.guid;
       document.querySelector("#press_interview1").textContent = press.interview1;
       document.querySelector("#press_interview_year1").textContent = press.interview_year1;
 
-      document.querySelector("#press_photo_reportage").textContent = press.photo_reportage;
 
+
+      document.querySelector("#press_photo_reportage").textContent = press.photo_reportage;
+      document.querySelector("#reportage_img").src = press.reportage_img.guid;
       document.querySelector("#press_reportage1").textContent = press.reportage1;
       document.querySelector("#press_reportage_year1").textContent = press.reportage_year1;
+
+
       document.querySelector("#press_grants").textContent = press.grants;
+      document.querySelector("#grants_img").src = press.grants_img.guid;
       document.querySelector("#press_grants1").textContent = press.grants1;
       document.querySelector("#press_grants_year1").textContent = press.grants_year1;
+
 
   }
