0c9fb072d25f83906077c76cbec7d193c92c6069
Rikke Gaardsøe <rikkegaardsoe@Rikkes-MacBook-Air.local>
2020-05-26 14:58:05 +0200

    home

--- homedweller.js
+++ homedweller.js
@@ -31,23 +31,7 @@ function vis(project) {
     document.querySelector("#photo5").src = project.img5.guid;
     document.querySelector("#photo6").src = project.img6.guid;
     document.querySelector("#photo7").src = project.img7.guid;
-    document.querySelector("#photo8").src = project.img8.guid;
-    document.querySelector("#photo9").src = project.img9.guid;
-    document.querySelector("#photo10").src = project.img10.guid;
-    document.querySelector("#photo11").src = project.img11.guid;
-    document.querySelector("#photo12").src = project.img12.guid;
-    document.querySelector("#photo13").src = project.img13.guid;
-    document.querySelector("#photo14").src = project.img14.guid;
-    document.querySelector("#photo15").src = project.img15.guid;
-    document.querySelector("#photo16").src = project.img16.guid;
-    document.querySelector("#photo17").src = project.img17.guid;
-    document.querySelector("#photo18").src = project.img18.guid;
-    document.querySelector("#photo19").src = project.img19.guid;
-    document.querySelector("#photo20").src = project.img20.guid;
-    document.querySelector("#photo21").src = project.img21.guid;
-    document.querySelector("#photo22").src = project.img22.guid;
-    document.querySelector("#photo23").src = project.img23.guid;
-    document.querySelector("#photo24").src = project.img24.guid;
+
     document.querySelector("#project_tekst2").textContent = project.tekst2;
     document.querySelector("#project_tekst3").textContent = project.tekst3;
     document.querySelector("#project_tekst4").textContent = project.tekst4;
