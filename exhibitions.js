9ff93b7d5e82cbb7361823fd4cb89e60f365acca
loui161h <55078332+loui161h@users.noreply.github.com>
2020-06-02 12:03:46 +0200

    exhibitons in progress

--- exhibitions.js
+++ exhibitions.js
@@ -23,7 +23,14 @@
       /* HUSK VARIBEL HER */
       document.querySelector("#exhibition_heading2").textContent = exhibition.current;
       document.querySelector("#poster1").src = exhibition.current_exhibition1.guid;
+      document.querySelector("#where_exhibtion1").textContent = exhibition.where_exhibtion1;
+      document.querySelector("#when_exhibtion1").textContent = exhibition.when_exhibtion1;
       document.querySelector("#poster2").src = exhibition.current_exhibition2.guid;
+      document.querySelector("#where_exhibtion2").textContent = exhibition.where_exhibtion2;
+      document.querySelector("#when_exhibtion2").textContent = exhibition.when_exhibtion2;
       document.querySelector("#poster3").src = exhibition.current_exhibition3.guid;
+      document.querySelector("#where_exhibtion3").textContent = exhibition.where_exhibtion3;
+      document.querySelector("#when_exhibtion3").textContent = exhibition.when_exhibtion3;
+
 
   }
